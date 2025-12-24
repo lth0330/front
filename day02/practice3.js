b33b64e85d4f44337b0b84275d81855018eb4908
lth0330 <lth0330@gmail.com>
2025-12-24 18:09:25 +0900

    2025-12-24 수정4

--- day02/practice3.js
+++ day02/practice3.js
@@ -1,2 +1,21 @@
 
-prompt()
+
+
+console.log("[주문접수]");
+
+
+alert("-이름 입력-"); 
+let name = prompt();
+console.log("-이름-");
+console.log(name);
+
+alert("-원하는 음료-"); 
+let drink = prompt();
+console.log("-음료-");
+console.log(drink);
+
+alert("-수량-")
+console.log("-수량-");
+let num = prompt();
+console.log(num);
+
