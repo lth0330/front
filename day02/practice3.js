64a52449de1b9b1de238458c843638c49e391be0
lth0330 <lth0330@gmail.com>
2025-12-24 18:18:02 +0900

    2025-12-24 최종

--- day02/practice3.js
+++ day02/practice3.js
@@ -1,21 +1,21 @@
 
+console.log("[주문접수]");    
 
 
-console.log("[주문접수]");
-
-
-alert("-이름 입력-"); 
-let name = prompt();
+alert("-이름 입력-");   // 질문 알람
+let name = prompt();   // 이름 입력
 console.log("-이름-");
 console.log(name);
 
-alert("-원하는 음료-"); 
-let drink = prompt();
+
+alert("-원하는 음료-");   // 질문 알람
+let drink = prompt();   // 음료 입력
 console.log("-음료-");
 console.log(drink);
 
-alert("-수량-")
-console.log("-수량-");
+
+alert("-수량-")         // 질문 입력 
+console.log("-수량-");  // 수량 입력
 let num = prompt();
 console.log(num);
 
