11a1be46602f2996c1a2330cfb1828ad62e7e9cd
lth0330 <lth0330@gmail.com>
2025-12-24 17:56:18 +0900

    2025-12-24 수정3

--- day02/practice2.js
+++ day02/practice2.js
@@ -2,19 +2,20 @@
 
 
 // 숫자 자료(가격, 총 결제 금액, 주문수량)
-console.log(2000);
-console.log(3000);
-console.log(3500);
-console.log(4000);
+console.log(2000);   // 가격
+console.log(3000);   // 가격
+console.log(5000);   // 총결제금액
+console.log(2);      //주문수량
 
 // 문자열 자료(제품명, 카테고리명, 주문번호)
-console.log("아메리카노");
-console.log("빽사이즈 아메리카노(ICED)")
-console.log("원조커피");
+console.log("아메리카노");                 //제품명
+console.log("빽사이즈 아메리카노(ICED)")    //카테고리명 
+console.log("원조커피");                  //
 
 // 블리언 자료
-console.log("품절");
-
+const 품절여부 = true; console.log(품절여부);
+const 결제여부 = true; console.log(결제여부);
+const 적립여부 = false; console.log(적립여부);
 
 // 배열 자료 (카테고리명들, 제품명들, 주문한 상품들)
 let 카테고리명들 = ["커피","음료","주스/에이드"];
@@ -26,6 +27,12 @@ console.log(주문한상품들);
 
 
 // 객체 자료(제품과 가격은 하나의 쌍,)
-console.log({제품명:"콜라", 가격:1000});
-console.log({선택된제품:[], 총주문금액:1000});
-console.log({주문번호:10, 결제금액:3500, 메뉴이름:"커피"});
+
+let 제품정보 = {제품명:"콜라", 가격:1000};
+let 결제창 = {선택된제품:[], 총주문금액:1000};
+let 출력정보 = {주문번호:10, 결제금액:3500, 메뉴이름:"커피"};
+console.log(제품정보);
+console.log(결제창);
+console.log(출력정보);
+
+
