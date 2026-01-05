4c29b59b917ead498149512598806850898f63c3
lth0330 <lth0330@gmail.com>
2026-01-05 15:03:26 +0900

    2026_01_05 오후

---
+++ day08/실습5.js
@@ -0,0 +1,12 @@
+let userNames=['김하준','이서하','박민솔','최도윤']
+for(let a=0; a <= userNames.length-1; a++ ){
+  if(userNames[a].indexOf('솔') != -1){
+    console.log(userNames[a]);
+    break;
+  }
+  
+}
+
+
+
+
